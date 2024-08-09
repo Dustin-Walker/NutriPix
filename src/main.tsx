8886d0f2469a715b6a2215f2a3d4472bbe4296ca
dustin-walker <Honeywell12##>
2024-08-09 13:21:39 -0700

    Installed react-router-dom. Set up browser router on application with a few routes.

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <article>Home page!</article>
+    },
+    {
+        path: "/log-in",
+        element: <article>Log in page!</article>
+    },
+    {
+        path: "/snap",
+        element: <article>Snap a meal!</article>
+    },
+    {
+        path: "/my-profile",
+        element: <article>My profile!</article>
+    },
+    {
+        path: "/meal-planner",
+        element: <article>Meal planner!</article>
+    }
+    ])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
