70e3e06dd2d5b1a800ca6f16d7cb9c84190d9b31
dustin-walker <dustinwalker01@gmail.com>
2024-08-10 20:13:00 -0700

    Removed unused routes

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Snap from './pages/snap/Snap.tsx';
-import UserProfile from "./pages/social/user-profile.tsx";
-import MealPlanner from "./pages/meal-planner/meal-planner.tsx";
 import Home from "./pages/home/Home.tsx";
 import Login from "./pages/login/Login.tsx";
 
@@ -21,15 +19,7 @@ const router = createBrowserRouter([
         path: "/snap",
         element: <Snap />
     },
-    {
-        path: "/my-profile",
-        element: <UserProfile />
-    },
-    {
-        path: "/meal-planner",
-        element: <MealPlanner />
-    }
-    ])
+])
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
