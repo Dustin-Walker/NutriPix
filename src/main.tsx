15c77849372b6d98b674f2f1fb381b57a5723cf2
dustin-walker <dustinwalker01@gmail.com>
2024-08-11 09:11:01 -0700

    Setting base name in the router

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import Snap from './pages/snap/Snap.tsx';
-import Home from "./pages/home/Home.tsx";
-import Login from "./pages/login/Login.tsx";
-
-const router = createBrowserRouter([
-    {
-        path: "/",
-        element: <Home />
-    },
-    {
-        path: "/login",
-        element: <Login />
-    },
-    {
-        path: "/snap",
-        element: <Snap />
-    },
-])
+import App from "./App.tsx";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+      <App />
   </StrictMode>,
 )
