a30c625bc1d528670fb3a055faf1da5482135d44
dustin-walker <dustinwalker01@gmail.com>
2024-08-10 20:36:58 -0700

    Changing vite config base

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/NutriPix/"
+  base: "https://dustin-walker.github.io/NutriPix/"
 })
