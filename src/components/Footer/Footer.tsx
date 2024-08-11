6c7ecb1d1767b61f448d8e0b84c1c05d25b584c7
dustin-walker <dustinwalker01@gmail.com>
2024-08-10 19:44:11 -0700

    Added login button

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -4,7 +4,7 @@ import changeRoute from "../../utils/changeRoute.ts";
 const Footer = () => (
     <footer className="footer">
         <button onClick={(e) => changeRoute(e, '')}>Home</button>
-        <button>Notes</button>
+        <button onClick={(e) => changeRoute(e, 'login')}>Log In</button>
         <button onClick={(e) => changeRoute(e, 'snap')}>Snap</button>
         <button>Plate</button>
         <button>Profile</button>
